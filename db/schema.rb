f2f8e4abffb4050f2f74bca584c24aca6641f676
EC2 Default User <ec2-user@ip-172-31-17-59.ap-northeast-2.compute.internal>
2016-02-21 11:20:01 +0000

    apache2

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110063028) do
-
-  create_table "activity_images", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "image"
+ActiveRecord::Schema.define(version: 0) do
 
 end
