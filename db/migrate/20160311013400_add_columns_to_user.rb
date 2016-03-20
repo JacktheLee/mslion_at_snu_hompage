4bb52c1c8e0f0c8ba46597e8c7385d38ac014068
JungYuCheol <jungyucheol@JungYuCheolui-MacBook-Pro.local>
2016-03-20 19:38:07 +0900

    조짜기 만들다 잠시 주토피아 보고 오겠음

--- db/migrate/20160311013400_add_columns_to_user.rb
+++ db/migrate/20160311013400_add_columns_to_user.rb
@@ -3,6 +3,5 @@ class AddColumnsToUser < ActiveRecord::Migration
     add_column :users, :name, :string
     add_column :users, :score, :integer, default: 0
     add_column :users, :authority, :string, default: "student"
-    add_column :users, :team_id, :integer
   end
 end
