588ee8c21ffc66f9543c6301d651163627bf4981
jackthelee <ljs8860@snu.ac.kr>
2016-03-20 12:02:48 +0000

    뻘짓중

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,9 @@ class User < ActiveRecord::Base
   has_many :user_assignments
   has_many :assignments, through: :user_assignments
 
-<<<<<<< HEAD
   belongs_to :team
-=======
   has_many :user_teams
   has_many :teams, through: :user_teams
->>>>>>> b801a4f23cae0234c49fc7e14c078aa72679ca26
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- db/migrate/20160311013400_add_columns_to_user.rb
+++ db/migrate/20160311013400_add_columns_to_user.rb
@@ -3,9 +3,7 @@ class AddColumnsToUser < ActiveRecord::Migration
     add_column :users, :name, :string
     add_column :users, :score, :integer, default: 0
     add_column :users, :authority, :string, default: "student"
-<<<<<<< HEAD
     add_column :users, :team_id, :integer
-=======
->>>>>>> b801a4f23cae0234c49fc7e14c078aa72679ca26
+
   end
 end
