4ae3f3ccd700626c87afb843f1354be3df68042a
JungYuCheol <jungyucheol@JungYuCheolui-MacBook-Pro.local>
2016-03-20 17:53:06 +0900

    aws키 노출하지 마아아

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:      "AKIAJLNZKPMSP5CCHPAQ",                        # required
-    aws_secret_access_key:  "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",                        # required
+    aws_access_key_id:      ENV["AWS_ACCESS_KEY"],                        # required
+    aws_secret_access_key:  ENV["AWS_SECRET_ACCESS_KEY"],                        # required
     region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'likelion-snu'                          # required
