53b241f2ba5ff9a1dcdfc441ee437e0e0256fc35
NamHyeongKeol <grey_pastor@naver.com>
2016-03-31 23:35:53 +0900

    handle error in assignment_controller

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -19,7 +19,7 @@ class AssignmentController < ApplicationController
   end
 
   def board
-     @test_assigntitle= AssignmentTitle.where(name: "Code Academy").take
+     @test_assigntitle= AssignmentTitle.where(name: "Codeacademy 1").take
 
   end
   
